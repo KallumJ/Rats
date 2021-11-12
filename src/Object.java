e4aba01e118529b93899613d28f5ddaaa594c2be
Fahd17 <fahdsattam8@gmail.com>
2021-11-12 16:22:50 +0000

    Update Object.java
    added the constructor of Object

--- src/Object.java
+++ src/Object.java
@@ -11,4 +11,13 @@ public abstract class Object {
     // Loaded icon of the object
     private Image icon;
     
+    // The Tile the objetc is currently standing on
+    private Tile standingOn;
+    
+    protected Object (Tile standingOn, Image icon){
+        
+        this.icon = icon;
+        this.standingOn = standingOn;   
+    }
+    
 }
