c7d70dcbb97cb9004ba9aa4a807a6b9144879904
Fahd17 <fahdsattam8@gmail.com>
2021-11-12 17:00:07 +0000

    Update Rat.java
    added attributes of Rat

--- src/Rat.java
+++ src/Rat.java
@@ -7,6 +7,11 @@ import javafx.scene.image.Image;
  * @author fahds
  */
 public abstract class Rat extends Object{
+    
+    // movement speed of the Rat
+    private int speed; 
+    
+    private Direction directionOfMovement;
 
     public Rat(Tile standingOn, Image icon) {
         super(standingOn, icon);
