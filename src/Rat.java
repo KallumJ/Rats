c4191051587ecab48b5c15ff39f8370b32fb8ec9
Fahd17 <fahdsattam8@gmail.com>
2021-11-12 16:37:40 +0000

    Create Rat.java
    created Rat class

---
+++ src/Rat.java
@@ -0,0 +1,15 @@
+
+import javafx.scene.image.Image;
+
+
+/**
+ *
+ * @author fahds
+ */
+public abstract class Rat extends Object{
+
+    public Rat(Tile standingOn, Image icon) {
+        super(standingOn, icon);
+    }
+    
+}
