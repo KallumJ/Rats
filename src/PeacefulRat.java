60c53ef59a46840a8dacd57473d883e6f68d5774
Fahd17 <fahdsattam8@gmail.com>
2021-11-15 12:20:49 +0000

    added constructor

--- src/PeacefulRat.java
+++ src/PeacefulRat.java
@@ -1,9 +1,12 @@
 
+import javafx.scene.image.Image;
+
+
 /**
  *
  * @author fahds
  */
-public class PeacefulRat {
+public class PeacefulRat extends Rat{
     
     private boolean sterile;
     private boolean adult;
@@ -11,4 +14,48 @@ public class PeacefulRat {
     private String gender;
     private int timeToGiveBirth;
     private int timeToDevelop;
+    
+    public PeacefulRat (Tile standingOn,  boolean sterile, boolean adult, boolean pregnant, String gender,
+                        int timeToGiveBirth, int timeToDevelop, int speed, Direction directionOfMovement) {
+        super(standingOn, null, speed,  directionOfMovement);
+        
+        Image icon = decideIcon(adult, pregnant, gender);
+        this.changeIcon(icon);
+        
+        this.sterile  = sterile;
+        this.adult = adult;
+        this.pregnant = pregnant;
+        this.gender = gender;
+        this.timeToDevelop = timeToDevelop;
+        this.timeToGiveBirth = timeToGiveBirth;    
+    }
+    
+    private Image decideIcon (boolean adult, boolean pregnant, String gender) {
+        
+        Image decidedIcon = null;
+        
+        if (!adult) {
+            
+           // decidedIcon = babyRat.png;
+        } 
+        else if (gender.equalsIgnoreCase("Male") ){
+            
+            // decidedIcon = AdultMaleRat.png
+        }
+        else if (pregnant) {
+            
+            //decidedIcon = pregnantFemaleRat.png;  
+        }
+        else {
+            
+            //decidedIcon = femaleRat.png
+        }
+        
+        return decidedIcon;
+    }
+
+    @Override
+    public void changeIcon(Image icon) {
+        
+    }
 }
