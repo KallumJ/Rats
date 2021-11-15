e9ba7e230c267c8c2b53e38b8f1e719191773f90
Fahd17 <fahdsattam8@gmail.com>
2021-11-15 20:12:03 +0000

    created deathRat class

---
+++ src/DeathRat.java
@@ -0,0 +1,52 @@
+
+import javafx.scene.image.Image;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author fahds
+ */
+public class DeathRat extends Rat {
+    
+    private int numberOfKills;
+    private int killsTarget;
+    
+
+    public DeathRat(Tile standingOn, Image icon, int speed, Direction directionOfMovement,
+                   int numberOfKills, int killsTarget) {
+        super(standingOn, icon, speed, directionOfMovement);
+        
+        this.killsTarget = killsTarget;
+        this.numberOfKills = numberOfKills;
+    }
+
+    @Override
+    public void changeIcon(Image icon) {
+
+    }
+    
+    public void kill (Rat victom, Board board) {
+        
+        board.removeObject(victom);
+        this.numberOfKills = this.numberOfKills + 1;
+        
+        if (this.killsTarget == this.numberOfKills) {
+           
+            board.removeObject(this);
+        }
+        else {
+            
+        } 
+        
+    }
+    
+    public int getNumberOfKills () {
+        
+         return this.numberOfKills;
+    }
+    
+}
