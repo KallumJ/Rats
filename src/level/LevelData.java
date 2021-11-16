4c01f3dae68104b4364fe65673fd560115a15b55
Kallum Jones <57623845+KallumJ@users.noreply.github.com>
2021-11-16 23:38:46 +0000

    Added objects list to LevelData

--- src/level/LevelData.java
+++ src/level/LevelData.java
@@ -1,14 +1,19 @@
 package level;
 
+import objects.Object;
 import tile.TileSet;
+
+import java.util.ArrayList;
 
 public class LevelData {
     private final LevelProperties levelProperties;
     private final TileSet tileSet;
+    private final ArrayList<Object> objects;
 
-    public LevelData(LevelProperties levelProperties, TileSet tileSet) {
+    public LevelData(LevelProperties levelProperties, TileSet tileSet, ArrayList<Object> objects) {
         this.levelProperties = levelProperties;
         this.tileSet = tileSet;
+        this.objects = objects;
     }
 
     public LevelProperties getLevelProperties() {
@@ -17,5 +22,9 @@ public class LevelData {
 
     public TileSet getTileSet() {
         return tileSet;
+    }
+
+    public ArrayList<Object> getObjects() {
+        return objects;
     }
 }
