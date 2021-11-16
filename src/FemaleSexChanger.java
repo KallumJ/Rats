c0250334d82a791e97a19f2a765097442d02237a
Fahd17 <fahdsattam8@gmail.com>
2021-11-16 14:05:35 +0000

    Create FemaleSexChanger.java

---
+++ src/FemaleSexChanger.java
@@ -0,0 +1,34 @@
+
+import javafx.scene.image.Image;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author fahds
+ */
+public class FemaleSexChanger extends Item{
+
+    public FemaleSexChanger(Tile standingOn) {
+        super(standingOn, null);
+    }
+
+    @Override
+    public void activition(Board board, Rat rat) {
+        
+        PeacefulRat pRat = (PeacefulRat) rat;
+        pRat.setGender("Female");
+        
+        board.removeObject(this);   
+    }
+
+    @Override
+    public void changeIcon(Image icon) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
