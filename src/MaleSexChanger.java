06e614fd0668919496d7dd9761529cfb0c5f3532
Fahd17 <fahdsattam8@gmail.com>
2021-11-16 14:05:42 +0000

    Create MaleSexChanger.java

---
+++ src/MaleSexChanger.java
@@ -0,0 +1,29 @@
+
+import javafx.scene.image.Image;
+
+
+/**
+ *
+ * @author fahds
+ */
+public class MaleSexChanger extends Item{
+    
+    public MaleSexChanger(Tile standingOn) {
+        super(standingOn, null);
+    }
+
+    @Override
+    public void activition(Board board, Rat rat) {
+        
+        PeacefulRat pRat = (PeacefulRat) rat;
+        pRat.setGender("Male");
+        
+        board.removeObject(this);   
+    }
+
+    @Override
+    public void changeIcon(Image icon) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
