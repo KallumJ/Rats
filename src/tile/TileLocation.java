75e65b0482b15aeeee71890183f577227a72ccb5
Kallum Jones <57623845+KallumJ@users.noreply.github.com>
2021-11-16 22:20:52 +0000

    Added TileLocation class

---
+++ src/tile/TileLocation.java
@@ -0,0 +1,20 @@
+package tile;
+
+public class TileLocation {
+    private int x;
+    private int y;
+
+    public TileLocation(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
