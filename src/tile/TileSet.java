306dc84138e1f3c4add8ed8121adcd5a62014496
Kallum Jones <57623845+KallumJ@users.noreply.github.com>
2021-11-16 22:31:12 +0000

    Added TileSet class

---
+++ src/tile/TileSet.java
@@ -0,0 +1,19 @@
+package tile;
+
+import java.util.ArrayList;
+
+public class TileSet {
+    private final ArrayList<ArrayList<Tile>> tileSet = new ArrayList<>();
+
+    public void putTile(Tile tile, int x, int y) {
+        if (tileSet.get(y) == null) {
+            tileSet.add(y, new ArrayList<>());
+        }
+
+        tileSet.get(y).add(x, tile);
+    }
+
+    public Tile getTile(int x, int y) {
+        return tileSet.get(y).get(x);
+    }
+}
