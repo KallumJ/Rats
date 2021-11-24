47e6de48575bfc142b12564ccc71cc0d747407ac
KatsuPatrick <matthewsonofstephen@gmail.com>
2021-11-24 17:45:49 +0000

    Extends MoTD to solve current puzzle
    Reworks and extends MoTD, compressing puzzle request into a
    single method, and implementing solver methods that generate
    the solution from a given puzzle.
    
    Includes intial outline for the sole public-facing method, getMoTD.

--- src/motd/MoTD.java
+++ src/motd/MoTD.java
@@ -1,31 +1,66 @@
 package motd;
 
-import java.io.*;
-import java.time.*;
-import java.net.http.*;
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.net.URI;
+import java.time.Duration;
 
 public class MoTD {
     private static final URI PUZZLEURI = URI.create("http://cswebcat.swansea.ac.uk/puzzle");
 
-    private static HttpResponse getPuzzleResponse() throws IOException, InterruptedException {
+    private static String getPuzzle() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1)
                 .connectTimeout(Duration.ofSeconds(20)).build();
         HttpRequest puzzleRequest = HttpRequest.newBuilder().uri(PUZZLEURI).build();
-        return client.send(puzzleRequest, HttpResponse.BodyHandlers.ofString());
+        HttpResponse response = client.send(puzzleRequest, HttpResponse.BodyHandlers.ofString());
+        return response.body().toString();
     }
 
-    private static String getPuzzle() {
+    private static char charShift(char ch, int shift) {
+        final int AZLENGTH = 26;
+        final int VALUE_A = (int) 'A';
+        int charIndex = ch - VALUE_A;
+        charIndex = (charIndex + shift) % AZLENGTH;
+        if (charIndex < 0) {
+            charIndex += AZLENGTH;
+        }
+        char newChar = (char) (VALUE_A + charIndex);
+        return newChar;
+    }
+
+    private static String solvePuzzle(String puzzle) {
+        String solution = "";
+        for (int i = 1; i <= puzzle.length(); i++) {
+            int shift = ((int) Math.pow(-1, i)) * i;
+            char shiftedChar = charShift(puzzle.charAt(i - 1), shift);
+            solution += shiftedChar;
+        }
+        solution += "CS-230";
+        solution = solution.length() + solution;
+        return solution;
+    }
+
+    public static String getMoTD() {
         String puzzle = "";
         try {
-            puzzle = getPuzzleResponse().body().toString();
+            puzzle = getPuzzle();
         } catch (IOException e) {
             System.out.println(e);
-            puzzle = "IO_ERROR";
+            return "IO_ERROR";
         } catch (InterruptedException e) {
             System.out.println(e);
-            puzzle = "INTERRUPT_ERROR";
+            return "INTERRUPT_ERROR";
         }
-        return puzzle;
+
+        String solution = solvePuzzle(puzzle);
+        System.out.println(solution);
+        return "";
     }
+
+    public static void main(String args[]) {
+        String temp = getMoTD();
+    }
+
 }
