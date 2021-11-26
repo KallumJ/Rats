3344c42068dd3a8249ad19248ac0860ac5c6ee23
Kallum <kallummj071001@gmail.com>
2021-11-26 19:56:33 +0000

    Added ObjectUtils to provide utility methods to do with getting values about objects

---
+++ src/objects/ObjectUtils.java
@@ -0,0 +1,45 @@
+package objects;
+
+import tile.Tile;
+
+public class ObjectUtils {
+
+    public static String getObjectImageUrl(GameObjectType type) {
+        switch (type) {
+            case BOMB:
+                return "file:resources/bomb.png";
+            case STERILISATION:
+                return "file:resources/sterilistation.png";
+            case NO_ENTRY_SIGN:
+                return "file:resources/noEntrySign.png";
+            case POISON:
+                return "file:resources/poison.png";
+            case FEMALE_SEX_CHANGER:
+                return "file:resources/femaleChange.png";
+            case MALE_SEX_CHANGER:
+                return "file:resources/maleChange.png";
+            default:
+                throw new IllegalArgumentException("There is no image assigned to " + type);
+        }
+    }
+
+    public static GameObject getObjectFromType(Tile standingOn, GameObjectType type) {
+        switch (type) {
+            case BOMB:
+                return new Bomb(standingOn, Bomb.DEFAULT_LENGTH, false);
+            case STERILISATION:
+                return new Sterilisation(standingOn, Sterilisation.DEFAULT_DURATION);
+            case NO_ENTRY_SIGN:
+                return new NoEntrySign(standingOn, NoEntrySign.DEFAULT_DAMAGE_DONE, NoEntrySign.DEFAULT_DURABILITY);
+            case POISON:
+                return new Poison(standingOn);
+            case FEMALE_SEX_CHANGER:
+                return new FemaleSexChanger(standingOn);
+            case MALE_SEX_CHANGER:
+                return new MaleSexChanger(standingOn);
+            default:
+                throw new IllegalArgumentException("There is no object constructor assigned to " + type);
+        }
+    }
+
+}
