c6b05f369d8f956ea7f36ee4e92bbe1a9df9bf62
Kallum <kallummj071001@gmail.com>
2021-11-26 17:01:03 +0000

    Added fix for NoEntrySign

--- src/objects/NoEntrySign.java
+++ src/objects/NoEntrySign.java
@@ -1,7 +1,10 @@
 package objects;
 
 import display.Board;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.image.Image;
+import javafx.util.Duration;
 import objects.rats.Rat;
 import tile.Tile;
 
@@ -23,6 +26,8 @@ public class NoEntrySign extends GameObject {
     private Image noEntrySignImage;
     private int damageDone;
     private int durability;
+    private boolean recentlyActivated;
+    private static final int DELAY = 1;
 
 	/** 
 	 * Number entry sign
@@ -45,18 +50,25 @@ public class NoEntrySign extends GameObject {
 	 * @param rat  the rat
 	 */
     public void activation(Rat rat) {
-       
-        rat.setDirectionOfMovement(rat.turnAround(rat.getDirectionOfMovement()));
-        this.damageDone = damageDone+1;
-        
-        if (damageDone >= durability) {
-            
-            GameObject.getBoard().removeObject(this);
-            
+
+        // If we haven't been activated recently
+        if (!recentlyActivated) {
+            recentlyActivated = true;
+
+            // after the specified delay has passed, allow damage to be done again
+            Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(DELAY), event -> recentlyActivated = false));
+            timeline.play();
+
+            rat.setDirectionOfMovement(rat.turnAround(rat.getDirectionOfMovement()));
+            this.damageDone = damageDone+1;
+
+            if (damageDone >= durability) {
+
+                GameObject.getBoard().removeObject(this);
+
+            }
+            GameObject.getBoard().updateBoardDisplay();
         }
-        GameObject.getBoard().updateBoardDisplay();
-        
-        
     }
     /** 
 	 * Gets the damage done
