69bb73a5395327ca5e3228ccdb8bf07d872b33fe
Fahd17 <fahdsattam8@gmail.com>
2021-11-29 20:46:57 +0000

    fixed coding conventions

--- src/objects/rats/DeathRat.java
+++ src/objects/rats/DeathRat.java
@@ -16,9 +16,9 @@ import tile.Tile;
  */
 /**
  * The class Death rat extends rat
- */ 
+ */
 public class DeathRat extends Rat {
-    
+
     private int numberOfKills;
     private int killsTarget;
     private Image deathRatImage;
@@ -26,49 +26,51 @@ public class DeathRat extends Rat {
     public static final Direction DEFAULT_DIRECTION = Direction.UP;
     public static final int DEFAULT_NUM_OF_KILLS = 0;
     public static final int DEFAULT_KILLS_TARGET = 5;
-    
-	/** 
-	 * Constructs Death rat
-	 * @param standingOn  the standing on
-	 * @param speed  the speed
-	 * @param directionOfMovement  the direction of movement
-	 * @param killsTarget  the kills target
-	 */
+
+    /**
+     * Constructs Death rat
+     *
+     * @param standingOn the standing on
+     * @param speed the speed
+     * @param directionOfMovement the direction of movement
+     * @param killsTarget the kills target
+     */
     public DeathRat(Tile standingOn, int speed, Direction directionOfMovement,
             int numberOfKills, int killsTarget) {
         super(standingOn, speed, directionOfMovement);
 
-       
         this.killsTarget = killsTarget;
         this.numberOfKills = numberOfKills;
-        
+
         deathRatImage = new Image("file:resources/deathRat.png");
         super.setIcon(deathRatImage);
     }
 
-	/**
-	 * Kill's the provided rat
-	 * @param victim  the victim
-	 */
-    public void kill (Rat victim) {
-        
+    /**
+     * Kill's the provided rat
+     *
+     * @param victim the victim
+     */
+    public void kill(Rat victim) {
+
         GameObject.getBoard().removeObject(victim);
         this.numberOfKills = this.numberOfKills + 1;
-        
+
         if (this.killsTarget <= this.numberOfKills) {
-           
+
             GameObject.getBoard().removeObject(this);
         }
-        
+
     }
-	
-    /** 
-	 * Gets the number of kills
-	 * @return the number of kills
-	 */
-    public int getNumberOfKills () {
-        
-         return this.numberOfKills;
+
+    /**
+     * Gets the number of kills
+     *
+     * @return the number of kills
+     */
+    public int getNumberOfKills() {
+
+        return this.numberOfKills;
     }
-    
+
 }
