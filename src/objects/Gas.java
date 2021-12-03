1af86956ec40849db94805080b3bb1993d453b86
Fahd17 <fahdsattam8@gmail.com>
2021-12-03 17:29:34 +0000

    Added gas (doesn't kill yet)

--- src/objects/Gas.java
+++ src/objects/Gas.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package objects;
 
 import java.util.ArrayList;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.image.Image;
+import javafx.util.Duration;
 import tile.Direction;
 import tile.Tile;
 
@@ -15,11 +12,11 @@ import tile.Tile;
  *
  * @author fahds
  */
-public class Gas extends GameObject{
+public class Gas extends GameObject {
 
-    public static final int DEFAULT_DURATION = 5;
-    public static final int DEFAULT_RANGE = 3;
-    private  int duration;
+    public static final int DEFAULT_DURATION = 16;
+    public static final int DEFAULT_RANGE = 4;
+    private int duration;
     private int counter;
     private int range;
     private Image gasImage;
@@ -27,37 +24,47 @@ public class Gas extends GameObject{
 
     public Gas(Tile standingOn, int duration, int range) {
         super(standingOn);
-        
-        this.duration =  duration;
+
+        this.duration = duration;
         this.range = range;
         counter = 0;
         gasEffects = new ArrayList<>();
-        gasImage = new Image ("file:resources/gas.png");
+        gasImage = new Image("file:resources/gas.png");
         super.setIcon(gasImage);
     }
-    
-    public void activateGas () {
+
+    public void activateGas() {
         GameObject.getBoard().removeObject(this);
-        
-        
+        expand(super.getStandingOn());
+        gasEffects.add(new GasEffect(super.getStandingOn(), (duration + 2), this));
+
     }
-    
-    public void expand (Tile tile){
-        
+
+    private void delayExpand(Tile tile) {
+
+        Timeline delayTimer = new Timeline(new KeyFrame(Duration.seconds(2), event -> expand(tile)));
+        delayTimer.play();
+    }
+
+    public void expand(Tile tile) {
+
         for (Direction direction : Direction.values()) {
-            
-            if (tile.getAdjacentTile(direction).isTraversable()){
-                gasEffects.add(new GasEffect(tile.getAdjacentTile(direction), duration-(counter*5), this));  
-                
-                if (counter < range){
-                expand(tile.getAdjacentTile(direction));
+
+            if (tile.getAdjacentTile(direction).isTraversable()) {
+                gasEffects.add(new GasEffect(tile.getAdjacentTile(direction), (duration - ((gasEffects.size() / 4) * 2)), this));
+
+                if (counter < range) {
+                    delayExpand(tile.getAdjacentTile(direction));
                 } else {
-                    // starts timer after timer finish start dissapates 
+                    for (GasEffect gasEffect : gasEffects) {
+
+                        gasEffect.disappear();
+                    }
                 }
             }
-            this.counter++;
-            
+
         }
+        this.counter++;
     }
 
     public int getDuration() {
--- src/objects/GasEffect.java
+++ src/objects/GasEffect.java
@@ -3,32 +3,46 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package objects;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.image.Image;
+import javafx.util.Duration;
 import tile.Tile;
 
 /**
  *
  * @author fahds
  */
-public class GasEffect extends GameObject{
-    
+public class GasEffect extends GameObject {
+
     private int duration;
     private Gas sourceGas;
     private Image gasEffectImage;
+    private Timeline disappearTimer;
 
     public GasEffect(Tile standingOn, int duration, Gas sourceGas) {
         super(standingOn);
-        
+
         this.duration = duration;
         this.sourceGas = sourceGas;
-        
-        gasEffectImage = new Image ("file:resources/gasEffect.png");
+
+        gasEffectImage = new Image("file:resources/gasEffect.png");
         super.setIcon(gasEffectImage);
-        
+
         GameObject.getBoard().addObject(this);
     }
-    
+
+    public void disappear() {
+
+        disappearTimer = new Timeline(new KeyFrame(Duration.seconds(duration), event -> removeEffect()));
+        disappearTimer.play();
+    }
+
+    private void removeEffect() {
+
+        GameObject.getBoard().removeObject(this);
+    }
+
 }
