4677a3a136dd42b0c96822a0ee4611872564af9c
sampinisetti2002 <93994958+sampinisetti2002@users.noreply.github.com>
2022-02-09 23:37:37 +0000

    Update MainMenu.java
    1. Made the MOTD Scrollable 
    2. Changed the logout and delete buttons
    3. Changed the back button 
    4. Name:... on the top left corner - Changed - Same place but larger

--- src/display/menus/MainMenu.java
+++ src/display/menus/MainMenu.java
@@ -25,6 +25,9 @@ public class MainMenu extends GameMenu {
 	private static final String LOGOUT_TEXT = "Log out";
 	private static final int DELETE_OFFSET = 20;
 	private static final int LOGOUT_OFFSET = 10;
+	private static final Font TITLE_FONT = Font.font(GameMenu.DEFAULT_FONT,
+		FontWeight.SEMI_BOLD, 15);
+
 
 	/**
 	 * A method to create a main menu with all the main menu items we need on
@@ -43,11 +46,13 @@ public class MainMenu extends GameMenu {
 				null);
 
 		// Add Motd and user name labels
+		// make this scrollable 
 		Label motdLabel = new Label(MssgOfTheDay.getMotD());
-		motdLabel.setFont(TextUtils.getFont(18));
+		motdLabel.setFont(Font.font(DEFAULT_FONT, FontWeight.SEMI_BOLD, 18));
 		motdLabel.setTextFill(Color.WHITE);
 
 		mainMenu.setBottom(motdLabel);
+		applyScrollingEffects(motdLabel);
 
 		// Create a container for name controls
 		HBox nameContainer = new HBox();
@@ -57,14 +62,24 @@ public class MainMenu extends GameMenu {
 
 		String playerName = player.getPlayerName();
 		Label nameLabel = new Label(String.format(NAME_LABEL, playerName));
-		nameLabel.setFont(TextUtils.getFont(18));
+		nameLabel.setFont(TextUtils.getFont(20));
 		nameLabel.setTextFill(Color.WHITE);
 
 		// Create buttons to delete and logout of the current player profile
 		Button deleteButton = new Button(DELETE_TEXT);
+		deleteButton.setFont(TITLE_FONT);
+		deleteButton.setStyle("-fx-background-color: black; -fx-text-fill: white; -fx-border-color: darkgrey; -fx-border-width: 1px;");
+		deleteButton.setPrefWidth(150);
+
 		Button logoutButton = new Button(LOGOUT_TEXT);
+		logoutButton.setFont(TITLE_FONT);
+		logoutButton.setStyle("-fx-background-color: black; -fx-text-fill: white; -fx-border-color: darkgrey; -fx-border-width: 1px;");
+		logoutButton.setPrefWidth(150);
+
 		logoutButton.setTranslateX(LOGOUT_OFFSET);
+		logoutButton.setTranslateY(1);
 		deleteButton.setTranslateX(DELETE_OFFSET);
+		deleteButton.setTranslateY(1);
 
 		logoutButton.setOnMousePressed(event -> {
 			Scene scene = new Scene(new LoginMenu().buildMenu());
@@ -85,4 +100,28 @@ public class MainMenu extends GameMenu {
 		// Return the constructed menu
 		return mainMenu;
 	}
+	
+	/**
+	 * Apply scrolling effects to the provided motd label.
+	 */
+	private void applyScrollingEffects(Label motdLabel) {
+		// Use Timeline, Key Value and Key Frame
+		// Scrolling Text Effects for text "ABOUT RATS"
+		double sceneWidth = getStage().getWidth();
+		double textWidth = motdLabel.getLayoutBounds().getWidth();
+
+		KeyValue initKeyValue =
+				new KeyValue(motdLabel.translateXProperty(),
+						sceneWidth);
+		KeyFrame initFrame = new KeyFrame(Duration.ZERO, initKeyValue);
+		KeyValue endKeyValue =
+				new KeyValue(motdLabel.translateXProperty(),
+						-3.5 * textWidth);
+		KeyFrame endFrame = new KeyFrame(Duration.seconds(15), endKeyValue);
+
+		Timeline timeline = new Timeline(initFrame, endFrame);
+		timeline.setCycleCount(Timeline.INDEFINITE);
+		timeline.play();
+	}
+
 }
