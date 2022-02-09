7f77893498e495181a8d87fb683965d69770494e
sampinisetti2002 <93994958+sampinisetti2002@users.noreply.github.com>
2022-02-09 23:39:24 +0000

    Update LoginMenu.java
    1. Fixed the layout of login button and input text
    2. Login should get a border around it when the mouse hovers over it

--- src/display/menus/LoginMenu.java
+++ src/display/menus/LoginMenu.java
@@ -1,5 +1,6 @@
 package display.menus;
 
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -7,6 +8,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import players.PlayerProfileManager;
 
 /**
@@ -17,11 +20,17 @@ import players.PlayerProfileManager;
 public class LoginMenu extends GameMenu {
 
 	private static final String MENU_TITLE = " L O G I N ";
-	private static final String NAME_INPUT_PLACEHOLDER = "Enter your name...";
-	private static final String LOGIN_BUTTON_TEXT = "Login";
+	private static final String NAME_INPUT_PLACEHOLDER = "Enter your name..."; 
+	private static final String LOGIN_BUTTON_TEXT = "E N T E R"; 
 	private static final int NAME_CHAR_LIMIT = 20;
+	private static final int INPUT_BOX_X = 110;
+	private static final int INPUT_BOX_Y = 300;
+	private static final int LOGIN_BUTTON_X = -25;
+	private static final int LOGIN_BUTTON_Y = 335;
 	private static final String NAME_TOO_LONG =
 			"Please choose a name with " + NAME_CHAR_LIMIT + " characters or " + "less";
+	private static final Font TITLE_FONT = Font.font(GameMenu.DEFAULT_FONT,
+			FontWeight.SEMI_BOLD, 20);
 
 	/**
 	 * A method to build the login menu.
@@ -37,10 +46,24 @@ public class LoginMenu extends GameMenu {
 		// input the user's name
 		HBox nameControlsContainer = new HBox();
 		TextField inputBox = new TextField();
-
 		inputBox.setText(NAME_INPUT_PLACEHOLDER);
+		inputBox.setStyle("-fx-background-color: black; -fx-text-fill: white; -fx-border-color: darkgrey; -fx-border-width: 1px;");
+		inputBox.setPrefWidth(170);
+
+		inputBox.setTranslateX(INPUT_BOX_X);
+		inputBox.setTranslateY(INPUT_BOX_Y);
 
 		Button loginButton = new Button(LOGIN_BUTTON_TEXT);
+		loginButton.setFont(TITLE_FONT);
+		loginButton.setStyle("-fx-background-color: black; -fx-text-fill: white;");
+		loginButton.setPrefWidth(100);
+
+		loginButton.setOnMouseEntered(event -> {
+			loginButton.setStyle("-fx-background-color: black; -fx-text-fill: white; -fx-border-color: darkgrey; -fx-border-width: 1px;");
+		});
+
+		loginButton.setTranslateX(LOGIN_BUTTON_X);
+		loginButton.setTranslateY(LOGIN_BUTTON_Y);
 
 		// When the login button is pressed, save their name, and change to
 		// the new screen
@@ -60,11 +83,8 @@ public class LoginMenu extends GameMenu {
 
 
 		// Add and align the controls
+
 		nameControlsContainer.getChildren().addAll(inputBox, loginButton);
-
-		nameControlsContainer.setTranslateX(100);
-		nameControlsContainer.setTranslateY(300);
-
 		getCenter().getChildren().add(nameControlsContainer);
 
 		// Return the constructed menu
