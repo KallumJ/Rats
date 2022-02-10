ec9d9720bd7ac8836504f36d8220532c352fc33f
Kallum <57623845+KallumJ@users.noreply.github.com>
2022-02-10 18:00:30 +0000

    Added functionality to generate and display the blank level data needed after the user selects a size for their custom level

--- src/display/Board.java
+++ src/display/Board.java
@@ -16,14 +16,11 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
 import javafx.util.Duration;
 import level.LevelData;
 import level.LevelProperties;
@@ -52,12 +49,10 @@ public class Board {
 	private static final String INFORMATION_LABEL_TEXT =
 			"Time elapsed: %d " + "seconds. Expected time: %d seconds. Score " + "%d. Population to lose " + "%d.";
 	private static final int INFO_LABEL_PADDING = 5; // in pixels
-	private static final String IMAGE_NOT_FOUND = "Image for tile type %s not "
-			+ "found";
 
 	private final LevelData levelData;
 	private final Label timerLabel;
-	private final Canvas canvas;
+	private final TileCanvas tileCanvas;
 	private final Inventory inventory;
 	private final Timeline interactionCheckTimeline;
 	private final Timeline gameLabelTimeline;
@@ -73,14 +68,7 @@ public class Board {
 	public Board(LevelData levelData) {
 		this.levelData = levelData;
 		LevelProperties levelProperties = levelData.getLevelProperties();
-
-		// Find the width and the height of the canvas, in pixels for this
-		// level
-		int width = levelProperties.getLevelWidth() * Tile.TILE_SIZE; // in
-		// pixels
-		int height = levelProperties.getLevelHeight() * Tile.TILE_SIZE;
-
-		this.canvas = new Canvas(width, height);
+		this.tileCanvas = new TileCanvas(levelData);
 
 		// Start an interaction checker to check for interactions between the
 		// objects
@@ -159,34 +147,10 @@ public class Board {
 	}
 
 	/**
-	 * A method to get the canvas of the board.
-	 *
-	 * @return the Canvas for the board.
-	 */
-	public Canvas getCanvas() {
-		return canvas;
-	}
-
-	/**
 	 * A method to update the canvas that represents the board.
 	 */
 	public void updateBoardDisplay() {
-
-		// Get the Graphic Context of the canvas. This is what we draw on.
-		GraphicsContext gc = canvas.getGraphicsContext2D();
-
-		// Clear canvas
-		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-
-		// Set the background to white.
-		gc.setFill(Color.WHITE);
-		gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-
-		// Display tiles on screen
-		displayTiles();
-
-		// Display the objects on screen
-		displayObjects();
+		tileCanvas.updateBoardDisplay();
 	}
 
 	/**
@@ -268,7 +232,7 @@ public class Board {
 		BorderPane root = new BorderPane();
 
 		// Place game board on screen
-		root.setLeft(canvas);
+		root.setLeft(tileCanvas.getCanvas());
 
 		// Add inventory
 		root.setCenter(inventory.buildInventoryGUI());
@@ -376,7 +340,7 @@ public class Board {
 	private Pane buildProgressBar() {
 
 		Pane ratPopulation = new Pane();
-		double height = canvas.getHeight() / populationToLose;
+		double height = tileCanvas.getCanvas().getHeight() / populationToLose;
 		for (int i = 0; i < populationToLose; i++) {
 			Rectangle cell = new Rectangle(Tile.TILE_SIZE, height);
 			cell.setFill(Color.GREY);
@@ -426,22 +390,6 @@ public class Board {
 	}
 
 	/**
-	 * Display the objects on the screens
-	 */
-	private void displayObjects() {
-		GraphicsContext gc = canvas.getGraphicsContext2D();
-
-		List<GameObject> objects = levelData.getObjects();
-
-		// draw all objects
-		for (GameObject object : objects) {
-			double x = object.getStandingOn().getX() * Tile.TILE_SIZE;
-			double y = object.getStandingOn().getY() * Tile.TILE_SIZE;
-			gc.drawImage(object.getIcon(), x, y);
-		}
-	}
-
-	/**
 	 * Checks whether the game has been won or lost at the moment the method is
 	 * called.
 	 */
@@ -483,35 +431,10 @@ public class Board {
 	}
 
 	/**
-	 * Displays the tiles in the level on the board
+	 * Returns the canvas for this board as a JavaFX Canvas
+	 * @return the JavaFX canvas for this board
 	 */
-	private void displayTiles() {
-		// Get the Graphic Context of the canvas. This is what we draw on.
-		GraphicsContext gc = canvas.getGraphicsContext2D();
-
-		List<Tile> tiles = levelData.getTileSet().getAllTiles();
-
-		// Decide what image to display for this tile
-		for (Tile tile : tiles) {
-			Image tileImage;
-			switch (tile.getTileType()) {
-				case GRASS:
-					tileImage = new Image("file:resources/grass.jpg");
-					break;
-				case PATH:
-					tileImage = new Image("file:resources/path.jpg");
-					break;
-				case TUNNEL:
-					tileImage = new Image("file:resources/tunnel.jpg");
-					break;
-				default:
-					throw new RuntimeException(String.format(IMAGE_NOT_FOUND,
-							tile.getTileType()));
-			}
-
-			double x = tile.getX() * Tile.TILE_SIZE;
-			double y = tile.getY() * Tile.TILE_SIZE;
-			gc.drawImage(tileImage, x, y);
-		}
+	public Canvas getCanvas() {
+		return tileCanvas.getCanvas();
 	}
 }
---
+++ src/display/CustomBoard.java
@@ -0,0 +1,30 @@
+package display;
+
+import javafx.scene.layout.BorderPane;
+import level.LevelData;
+
+/**
+ * A class to model a board in a level editor
+ */
+public class CustomBoard {
+    private final TileCanvas tileCanvas;
+
+    /**
+     * Constructs a custom board
+     * @param levelData the LevelData for the custom level
+     */
+    public CustomBoard(LevelData levelData) {
+        this.tileCanvas = new TileCanvas(levelData);
+    }
+
+    /**
+     * A method to build the board's GUI
+     * @return the GUI as a BorderPane
+     */
+    public BorderPane buildGUI() {
+        BorderPane root = new BorderPane();
+        root.setLeft(tileCanvas.getCanvas());
+
+        return root;
+    }
+}
